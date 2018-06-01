bb8e239eef25b396fc141c5d5fe3f212113164ab
meisen <sunmay2008@qq.com>
2018-06-01 11:25:56 +0800

    fix helps

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,9 +1,18 @@
 <?php
-namespace Helpers
+namespace Helpers;
 
+public $params = [];
 class Helpers{
-	public function get(){
-		echo "hello world";
+	public function __get($property_name)
+	{
+		retrun $this->params[$property_name] ?? "no_value";
+	}
+
+	public function __set($property_name,$property_value)
+	{
+
+		$this->$params[$property_name] = $property_value;
+
 	}
 }
 
