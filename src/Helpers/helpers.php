aa2711daa39914665f55d1d5cb15031c6023864e
meisen <sunmay2008@qq.com>
2018-06-01 11:42:07 +0800

    fix

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,17 +1,17 @@
 <?php
 namespace Helpers;
 
-public $params = [];
 class Helpers{
+	private $params = [];
 	public function __get($property_name)
 	{
-		retrun $this->params[$property_name] ?? "no_value";
+		return $this->params[$property_name] ?? "no_value";
 	}
 
 	public function __set($property_name,$property_value)
 	{
 
-		$this->$params[$property_name] = $property_value;
+		$this->params[$property_name] = $property_value;
 
 	}
 }
